349f3be7d6680322eae80f49344f0a33bb4f538c
vijnanu1802 <49672701+vijnanu1802@users.noreply.github.com>
2021-01-06 18:51:40 +0530

    hshs

--- Jenkinsfiles/helloworld.groovy
+++ Jenkinsfiles/helloworld.groovy
@@ -3,14 +3,7 @@
 pipeline {
     agent any
     stages {
-        stage('Git Checkout'){
-            steps{
-                gitCheckout(
-                    branch: "master",
-                    url: "https://github.com/vijnanu1802/jenkins-shared-library-framework.git"
-                    )
-            }
-        }
+       
         stage('build') {
             steps {
                 HelloWorld
