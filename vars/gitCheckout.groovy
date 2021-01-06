c96a3b17261a260d6463410f41e415556ff37a78
vijnanu1802 <49672701+vijnanu1802@users.noreply.github.com>
2021-01-06 18:49:23 +0530

    my

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,10 @@
 def call(Map stageParams) {
 
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
-  }
+checkout([
+    $class: 'GitSCM', 
+    branches: [[name: '*/master']], 
+    doGenerateSubmoduleConfigurations: false, 
+    extensions: [], 
+    submoduleCfg: [], 
+    userRemoteConfigs: [[url: 'https://github.com/vijnanu1802/jenkins-shared-library-framework.git']]])
+}
